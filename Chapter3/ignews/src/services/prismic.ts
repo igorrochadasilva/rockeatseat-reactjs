d136f99cf02ffa3202b4b5541cae14ab7bd283aa
igorsilva <igor.silva@performait.com>
2022-05-24 11:28:02 -0300

    Completed Working with CMS

--- Chapter3/ignews/src/services/prismic.ts
+++ Chapter3/ignews/src/services/prismic.ts
@@ -1,22 +1,12 @@
-import * as prismic from '@prismicio/client'
+import * as prismic from "@prismicio/client";
 
 // Fill in your repository name
-export const repositoryName = 'your-repo-name'
+export const repositoryName = process.env.PRISMIC_ENDPOINT;
 
-export const client = prismic.createClient(repositoryName, {
-  // If your repository is private, add an access token
-  accessToken: '',
+export default function getPrismicClient(req?: unknown) {
+  const client = prismic.createClient(repositoryName, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  });
 
-  // This defines how you will structure URL paths in your project.
-  // Update the types to match the Custom Types in your project, and edit
-  // the paths to match the routing in your project.
-  //
-  // If you are not using a router in your project, you can change this
-  // to an empty array or remove the option entirely.
-  routes: [
-    {
-      type: 'homepage',
-      path: '/',
-    },
-  ],
-})
+  return client;
+}
