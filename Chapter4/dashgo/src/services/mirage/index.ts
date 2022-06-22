1bc56c1eb791fca8c25e58ab53356253f0dab255
igorsilva <igor.silva@performait.com>
2022-06-22 16:27:20 -0300

    react query concepts completed

---
+++ Chapter4/dashgo/src/services/mirage/index.ts
@@ -0,0 +1,45 @@
+import { createServer, Factory, Model } from "miragejs";
+import { faker } from "@faker-js/faker";
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+    //gerar dados em massa com factories
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+
+    seeds(server) {
+      server.createList("user", 10);
+    },
+
+    routes() {
+      this.namespace = "api";
+      this.timing = 750;
+      this.get("/users");
+      this.post("/users");
+      this.namespace = "";
+      this.passthrough(); //todas as chamadas que forem para o endereço api, passem pelo mirage, e se não for identificado aqui, passem adiante.
+    },
+  });
+
+  return server;
+}
